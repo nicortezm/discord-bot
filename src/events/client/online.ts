fa109b5a723d06c8e7b230a7589a794e5f591fdc
nicortezm <nascorman@gmail.com>
2024-12-11 21:43:52 -0300

    feat: set presence activity when bot is online

--- src/events/client/online.ts
+++ src/events/client/online.ts
@@ -1,12 +1,22 @@
 import { Event } from '../../interfaces';
 import { handleButtons, handleCommands } from '../../handlers';
-
+import { ActivityType, PresenceData } from 'discord.js';
 export const event: Event<'ready'> = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log(`[CLIENT] ${client.user.username} is online`);
 
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'sida',
+          type: ActivityType.Streaming,
+          url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          
+        },
+      ],
+    });
     handleCommands(client);
     handleButtons(client);
   },
