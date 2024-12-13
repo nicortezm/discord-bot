551b43870caf35acd2946395fb2dcc9219e36482
nicortezm <nascorman@gmail.com>
2024-12-13 13:18:40 -0300

    feat: add kazagumo player and shoukaku queue system

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import { Client, Collection } from 'discord.js';
 import { Devs, envs } from './config';
 import { GlobClient } from './interfaces';
 import { handleEvents } from './handlers';
-
+import { Connectors } from 'shoukaku';
+import { Kazagumo, Plugins } from 'kazagumo';
 const client = new Client({
-  intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+  intents: [
+    'Guilds',
+    'GuildMembers',
+    'GuildMessages',
+    'MessageContent',
+    'GuildVoiceStates',
+  ],
 }) as GlobClient;
 
 client.config = Devs;
@@ -14,3 +21,24 @@ client.buttons = new Collection();
 handleEvents(client);
 
 client.login(envs.DISCORD_TOKEN);
+
+const Nodes = [
+  {
+    name: 'main',
+    url: '13.58.63.36:2333',
+    auth: 'youshallnotpass',
+    secure: false,
+  },
+];
+
+client.manager = new Kazagumo(
+  {
+    defaultSearchEngine: 'youtube',
+    send: (guildId, payload) => {
+      const guild = client.guilds.cache.get(guildId);
+      if (guild) guild.shard.send(payload);
+    },
+  },
+  new Connectors.DiscordJS(client),
+  Nodes
+);
--- src/interfaces/globClient.ts
+++ src/interfaces/globClient.ts
@@ -2,6 +2,7 @@ import type { Client, ClientEvents, Collection } from 'discord.js';
 import { Command } from './command';
 import { Devs } from '../config';
 import { Button } from './components';
+import { Kazagumo } from 'kazagumo';
 
 export type GlobClient = Client<true> & Extraclient;
 
@@ -10,7 +11,9 @@ interface Extraclient {
   events: Collection<keyof ClientEvents, () => void>;
   commands: Collection<string, Command>;
   buttons: Collection<string, Button>;
+  manager?: Kazagumo;
 }
+
 
 export interface Event<T extends keyof ClientEvents> {
   name: T;
