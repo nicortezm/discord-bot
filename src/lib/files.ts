05c0b965666d032e27b1fcb7d4619f47ac6ddf94
nicortezm <nascorman@gmail.com>
2024-12-13 13:18:55 -0300

    feat: delete gob dependency using filesystem now

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,11 +1,45 @@
-import { glob } from 'glob';
+// import { glob } from 'glob';
+
+// export async function loadFiles(dir: string): Promise<string[]> {
+//   const isDev = __filename.endsWith('.ts');
+//   const fullPath = `${process.cwd().replaceAll('\\', '/')}/${
+//     isDev ? 'src' : 'dist'
+//   }`;
+//   const files = await glob(`${fullPath}/${dir}/**/*.${isDev ? 'ts' : 'js'}`);
+//   files.forEach((file) => delete require.cache[require.resolve(file)]);
+//   return files;
+// }
+
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function loadFiles(dir: string): Promise<string[]> {
   const isDev = __filename.endsWith('.ts');
-  const fullPath = `${process.cwd().replaceAll('\\', '/')}/${
-    isDev ? 'src' : 'dist'
-  }`;
-  const files = await glob(`${fullPath}/${dir}/**/*.${isDev ? 'ts' : 'js'}`);
-  files.forEach((file) => delete require.cache[require.resolve(file)]);
+  const fullPath = path.resolve(process.cwd(), isDev ? 'src' : 'dist', dir);
+
+  const files: string[] = [];
+
+  const walkDir = async (currentPath: string) => {
+    try {
+      const entries = await fs.readdir(currentPath, { withFileTypes: true });
+
+      for (const entry of entries) {
+        if (entry.isDirectory()) {
+          await walkDir(path.resolve(currentPath, entry.name));
+        } else if (
+          path.extname(entry.name).toLowerCase() === (isDev ? '.ts' : '.js')
+        ) {
+          const filePath = path.resolve(currentPath, entry.name);
+          files.push(filePath);
+          delete require.cache[require.resolve(filePath)];
+        }
+      }
+    } catch (error) {
+      console.error(`Error reading directory ${currentPath}:`, error);
+    }
+  };
+
+  await walkDir(fullPath);
+
   return files;
 }
